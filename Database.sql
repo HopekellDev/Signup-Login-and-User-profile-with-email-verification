1cb4e13c1856d65cfaaf140108a51e76c0f7a014
Ezenwa Hopekell <46373141+HopekellDev@users.noreply.github.com>
2021-02-02 19:04:31 +0100

    Create Database.sql

---
+++ Database.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE IF NOT EXISTS `tbl_users` (
+`userID` int(11) NOT NULL AUTO_INCREMENT,
+`userName` varchar(100) NOT NULL,
+`userEmail` varchar(100) NOT NULL UNIQUE,
+`userPass` varchar(100) NOT NULL,
+`userRegdate DATETIME NOT NULL,
+`userStatus` enum('Y','N') NOT NULL DEFAULT 'N',
+`tokenCode` varchar(100) NOT NULL,
+PRIMARY KEY (`userID`)
+)
